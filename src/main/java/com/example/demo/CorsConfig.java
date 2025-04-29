ad5e2cd20c20704266631da41541de331e2b05a0
LeninMena <leninmena2016@gmail.com>
2025-04-28 19:39:42 -0500

    Configuración CORS para permitir solicitudes de diferentes orígenes en la API.

--- src/main/java/com/example/demo/CorsConfig.java
+++ src/main/java/com/example/demo/CorsConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+//Configuración CORS para permitir solicitudes de diferentes orígenes en la API.
 @Configuration
 public class CorsConfig {
 
