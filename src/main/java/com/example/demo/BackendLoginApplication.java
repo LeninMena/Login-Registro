2708307f0063b0c167cc2fe5d9f81ca338ede76b
LeninMena <leninmena2016@gmail.com>
2025-04-28 19:29:20 -0500

    Configuración inicial de la aplicación Spring Boot. Define el punto de entrada principal para ejecutar la aplicación

--- src/main/java/com/example/demo/BackendLoginApplication.java
+++ src/main/java/com/example/demo/BackendLoginApplication.java
@@ -3,13 +3,11 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Configuración inicial de la aplicación Spring Boot. Define el punto de entrada principal para ejecutar la aplicación.
 @SpringBootApplication
 public class BackendLoginApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BackendLoginApplication.class, args);
-        //error encontrado
-        
     }
- 
 }
