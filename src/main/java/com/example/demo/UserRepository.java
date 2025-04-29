bfc05f8187e0c692b5c3a151533e571dba5aed18
LeninMena <leninmena2016@gmail.com>
2025-04-28 21:08:49 -0500

    Servicio para manejar la lógica de negocio de usuarios. Incluye la lógica de registro, login y validación de contraseñas.

--- src/main/java/com/example/demo/UserRepository.java
+++ src/main/java/com/example/demo/UserRepository.java
@@ -1,4 +1,5 @@
 package com.example.demo;
+//"Repositorio JPA para realizar operaciones CRUD sobre la entidad 'User' en la base de datos."
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
